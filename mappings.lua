9461e34fd0f8e43abcf072e699d69fd414d58cb9
Piotr Bandurski <redroy44@gmail.com>
2023-07-20 22:58:37 +0200

    dap ui + sbt shell

--- mappings.lua
+++ mappings.lua
@@ -3,10 +3,12 @@
 -- Please use this mappings table to set keyboard mapping since this is the
 -- lower level configuration and more robust one. (which-key will
 -- automatically pick-up stored data by this setting.)
+local utils = require "astronvim.utils"
 return {
   -- first key is the mode
   n = {
     ["<leader>fM"] = { function() require('telescope').extensions.metals.commands() end, desc = "Metals" },
+    ["<leader>ts"] = { function() utils.toggle_term_cmd "sbt" end, desc = "ToggleTerm sbt" },
     -- second key is the lefthand side of the map
     -- mappings seen under group name "Buffer"
     ["<leader>bn"] = { "<cmd>tabnew<cr>", desc = "New tab" },
--- plugins/user.lua
+++ plugins/user.lua
@@ -1,4 +1,17 @@
 return {
+  {
+    "rcarriga/nvim-dap-ui",
+    config = function(plugin, opts)
+      -- run default AstroNvim nvim-dap-ui configuration function
+      require "plugins.configs.nvim-dap-ui"(plugin, opts)
+
+      -- disable dap events that are created
+      local dap = require "dap"
+      dap.listeners.after.event_initialized["dapui_config"] = function() require("dap").repl.toggle() end
+      dap.listeners.before.event_terminated["dapui_config"] = nil
+      dap.listeners.before.event_exited["dapui_config"] = nil
+    end,
+  },
   -- You can also add new plugins here as well:
   -- Add plugins, the lazy syntax
   -- "andweeb/presence.nvim",
